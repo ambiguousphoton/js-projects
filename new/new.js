26586be01c6d7e86fa4b5d28ff2502463ef7d762
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-17 19:00:54 +0530

    Add files via upload

---
+++ new/new.js
@@ -0,0 +1,19 @@
+let count = 0
+
+
+
+
+function increment() {
+    count += 1;
+    document.getElementById("count-el").innerText = count;
+}
+function decrement() {
+    count -= 1;
+    document.getElementById("count-el").innerText = count;
+    
+}
+function save() {
+
+
+}
+                                           
