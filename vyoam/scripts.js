6bc8640b154fb5951be60acb34ad9fb9c81dc621
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-21 20:03:06 +0530

    Add files via upload

---
+++ vyoam/scripts.js
@@ -0,0 +1,9 @@
+function addMore() {
+    let name = document.getElementById('name').value;
+    if (name = '') {
+        document.getElementById('error').innerHTML = "Please enter Value";
+    }
+    else{
+        
+    }
+}
