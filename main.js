abc7a87eaf2142822885195c946f913ba69ee2d9
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-09-16 20:19:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// DOM Manipulation
+
+// Get element by ID
+const title = document.getElementById('heading');
+console.log(title);
+
+// Get element by Class name 
+const list = document.getElementsByClassName('list-items')
+console.log(list)
+
+// Get element by tag name
+const tag = document.getElementsByTagName('div');
+console.log(tag);
+
+// query selector
+document.querySelector('div');
