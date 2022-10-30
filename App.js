49a6a0d27e32bc124c30ffd9976a7305b67544e7
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-10-30 22:10:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+import Comp from './comp1';
+
+function App() {
+  return (
+    <div className='window'>
+      <div className='heading'>
+        Whatsapp
+      </div>
+      <div className="comp1">
+        <Comp/>
+      </div>
+      <div className="comp2"></div>
+    </div>
+  );
+}
+
+export default App;
