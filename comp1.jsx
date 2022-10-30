19be42400e03125b3bc6d63c2384c48ef3ab2810
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-10-30 22:11:13 +0530

    Add files via upload

---
+++ comp1.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function Comp () {
+    return(
+     <div>
+        <div>
+             vyoam
+            <img src = "./vyoam.png" alt = "" />
+        </div>
+        <div >
+            <li>
+              
+            </li>
+        </div>
+     </div>
+    );
+}
+ 
+export  default Comp;
