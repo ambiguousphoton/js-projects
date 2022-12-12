40271a324773570d642e202706acd98b9d28b90f
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-12-12 23:17:33 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -9,7 +9,7 @@ tl.style.color = 'blue';
 const listitems = document.querySelectorAll('.list-items');
 for(i = 0; i < listitems.length ;i++)
 {
-    listitems[i].style.fontSize = '18px';
+    listitems[i].style.fontSize = '21px';
     listitems[i].style.backgroundColor = 'red'
 }
 console.log(tl)
