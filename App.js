28cddfee04f2618e1d07907a1baba8f330d3bb28
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-12-24 22:42:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 import Comp from './comp1';
-Om = 189
+Om = 18
 function App() {
   return (
     <div className='window'>
