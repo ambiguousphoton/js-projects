67f0854c59c10fdba0548ee38b436b073b66585c
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2023-04-01 23:59:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='window'>
       <div className='heading'>
-         Next Big Chat
+         Next Big Chat app
       </div>
       <div className="comp1">
         <Comp/>
